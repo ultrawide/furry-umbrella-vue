d7a0e5a679c10b46036a0f4a91ad9c2590de4ec4
ultrawide <colinchan02@gmail.com>
2020-06-23 17:04:05 -0700

    Add event listeners

--- index.js
+++ index.js
@@ -28,4 +28,23 @@ var app4 = new Vue({
       { text: 'Build something awesome' }
     ]
   }
+});
+
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Hello Vue.js!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+})
+
+var app6 = new Vue({
+  el: '#app-6',
+  data: {
+    message: 'Hello Vue!'
+  }
 })
