0d6bc5072e516015b66d768cb96cb9340340a310
ultrawide <colinchan02@gmail.com>
2020-06-25 13:48:23 -0700

    Add get and set example

--- computed-properties/index.js
+++ computed-properties/index.js
@@ -11,3 +11,23 @@ var vm = new Vue({
     }
   }
 })
+
+var vm = new Vue({ el: '#demo',
+  data: {
+    firstName: 'Foo',
+    lastName: 'Bar',
+  },
+  computed: {
+	  fullName: {
+		get: function() {
+			return this.firstName + ' ' + this.lastName
+		},
+		set: function(newValue) {
+			var names = newValue.split(' ')
+			this.firstName = names[0]
+			this.lastName = names[names.length - 1]
+	  } // end set
+	} // end full name
+  }
+})
+
