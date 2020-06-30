944bb6805595f099b97774d7f3e6769e6ce4df7a
ultrawide <colinchan02@gmail.com>
2020-06-29 18:55:41 -0700

    Add files

---
+++ forms/index.js
@@ -0,0 +1,36 @@
+var ex1 = new Vue({
+	el: '#example-1',
+	data: {
+		message: ''
+	}
+})
+var ex2 = new Vue({
+	el: '#example-2',
+	data: {
+		message: ''
+	}
+})
+var ex3 = new Vue({
+	el: '#example-3',
+	data: {
+		checked: false
+	}
+})
+var ex4 = new Vue({
+	el: '#example-4',
+	data: {
+		picked: 1
+	}
+});
+new Vue({
+  el: '#example-5',
+  data: {
+    selected: ''
+  }
+})
+new Vue({
+  el: '#example-6',
+  data: {
+    selected: ''
+  }
+})
