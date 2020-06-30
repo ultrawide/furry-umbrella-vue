c1a68725c5b5cdea474789afcdcca0cf8e90b907
ultrawide <colinchan02@gmail.com>
2020-06-29 19:14:25 -0700

    Add components template

---
+++ components/index.js
@@ -0,0 +1,12 @@
+// Define a new component called button-counter
+Vue.component('button-counter', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+})
+
+new Vue({ el: '#components-demo' })
+
